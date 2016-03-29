8c1741b03448602c74282cd61db1e2db6dbb8e39
Manolo Carrasco <manolo@vaadin.com>
2016-03-29 15:08:04 +0200

    Adding vaadin-icons

--- tasks/config.js
+++ tasks/config.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var userhome = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
 
 module.exports = {
-  coreElements: ['vaadin-grid', 'vaadin-combo-box', 'vaadin-date-picker', 'vaadin-upload'],
+  coreElements: ['vaadin-grid', 'vaadin-combo-box', 'vaadin-date-picker', 'vaadin-upload', 'vaadin-icons'],
   version: args.version || 'master',
   permalink: args.version ? 'latest' : '',
   toolsHost: args.toolsHostname || 'tools.vaadin.com',
