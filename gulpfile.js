0167fb458441a4465c4850e9b945632101d2afd6
Teemu Pöntelin <teemu@vaadin.com>
2016-05-30 14:26:07 +0300

    Add "zip" Gulp task to create the zip package for a release

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,9 @@ var git = require('gulp-git');
 var version = '0.3.0';
 
 gulp.task('default', function() {
-  console.log('\n  Use:\n gulp <stage|deploy[:cdn]>\n');
+  console.log('\n  Use:\n gulp <stage|deploy[:cdn]>|<zip>\n');
 });
 
-gulp.task('clean', ['clean:cdn']);
+gulp.task('clean', ['clean:cdn', 'clean:zip']);
 
 gulp.task('deploy', ['deploy:cdn']);
