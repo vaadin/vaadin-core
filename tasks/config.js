8a8118ed38d2d94a304f282e102a581eb72167fc
Manolo Carrasco <manolo@vaadin.com>
2016-08-24 12:14:27 +0200

    Adding vaadin-split-layout element to the collection

--- tasks/config.js
+++ tasks/config.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var userhome = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
 
 module.exports = {
-  coreElements: ['vaadin-grid', 'vaadin-combo-box', 'vaadin-date-picker', 'vaadin-upload', 'vaadin-icons'],
+  coreElements: ['vaadin-grid', 'vaadin-combo-box', 'vaadin-date-picker', 'vaadin-upload', 'vaadin-icons', 'vaadin-split-layout'],
   version: args.version || 'master',
   permalink: args.version ? 'latest' : '',
   toolsHost: args.toolsHostname || 'tools.vaadin.com',
